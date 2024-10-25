187bce0770f3fc76bb7fed8c13f8a8308760fdbc
Davi Passos <devdapacontact@gmail.com>
2024-10-25 00:06:14 -0300

    Navbar Estilo

--- src/assets/components/Navbar/HamburgerButton.jsx
+++ src/assets/components/Navbar/HamburgerButton.jsx
@@ -1,11 +1,18 @@
-function HamburgerButton (){
+import PropTypes from "prop-types"
+
+function HamburgerButton ({click, active}){
     return (
-        <button className="navbarHamburgerButton">
+        <button onClick={click} className={"navbarHamburgerButton " + active}>
             <span className="navbarHamburgerButtonBar"></span>
             <span className="navbarHamburgerButtonBar"></span>
             <span className="navbarHamburgerButtonBar"></span>
         </button>
     )
 }
+
+HamburgerButton.propTypes = {
+    click: PropTypes.func.isRequired, // Specify that click is a required function
+    active: PropTypes.string.isRequired, // Specify that click is a required function
+};
 
 export default HamburgerButton;
--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -1,17 +1,29 @@
 import { Link } from "react-router-dom";
 import HamburgerButton from "./HamburgerButton";
 import "./Navbar.css"
+import { useState } from "react";
 
 function Navbar(){
+
+    const [open, openState] = useState("")
+
+    function openNav(){
+        if (open == ""){
+            openState("active")
+        }else{
+            openState("")
+        }
+    }
+
     return (
 
         <header className="navbar">
             <Link to={"/"} className="navbarBrand">Dav.i</Link>
-            <HamburgerButton/>
-            <nav className="navbarNav">
+            <HamburgerButton click={openNav} active={open}/>
+            <nav className={"navbarNav " + open}>
                 <ul className="navbarNavList">
-                    <li className="navbarNavListItem"><Link className="navbarNavListItemLink" to={"/"}>Home</Link></li>
                     <li className="navbarNavListItem"><Link className="navbarNavListItemLink" to={"/Sobre"}>Sobre</Link></li>
+                    <li className="navbarNavListItem"><Link className="navbarNavListItemLink" to={"/"}>Home</Link></li>
                     <li className="navbarNavListItem"><Link className="navbarNavListItemLink" to={"/Projects"}>Projetos</Link></li>
                     <li className="navbarNavListItem"><Link className="navbarNavListItemLink" to={"/Skills"}>Habilidades</Link></li>
                     <li className="navbarNavListItem"><Link className="navbarNavListItemLink" to={"/Contact"}>Contato</Link></li>
